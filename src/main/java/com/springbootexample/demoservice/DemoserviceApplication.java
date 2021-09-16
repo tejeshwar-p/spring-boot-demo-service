9e7f4275543b9afb83ecc11bb890fafbb3d95ba3
Tejeshwar Pinjala <tejeshwar.p@gmail.com>
2021-09-16 13:37:12 +0530

    Initial commit | Tejeshwar

---
+++ src/main/java/com/springbootexample/demoservice/DemoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.springbootexample.demoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootexample/demoservice/DemoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootexample.demoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
